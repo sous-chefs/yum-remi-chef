6a4b88d4522678f0ab7fdc6b10289594cce37508
Sean OMeara <sean@chef.io>
2015-01-24 14:24:38 -0500

    updating metadata

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,9 @@ maintainer 'Chef'
 maintainer_email 'Sean OMeara <sean@chef.io>'
 license 'Apache 2.0'
 description 'Installs/Configures yum-remi'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
+
+supports 'centos'
+supports 'redhat'
 
 depends 'yum-epel'
