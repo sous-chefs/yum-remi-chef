83368d96e2a70dccbc87bbecfa5371ada21815bc
Tim Smith <tsmith84@gmail.com>
2015-09-07 22:04:20 -0700

    Fix metadata Fix name Expand OS support Add long_description Add supermarket metadata

--- metadata.rb
+++ metadata.rb
@@ -1,14 +1,16 @@
-name 'yum-remi-chef'
+name 'yum-remi'
 maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
 description 'Installs/Configures yum-remi'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.1'
 
-supports 'centos'
-supports 'redhat'
-supports 'oracle'
-supports 'scientific'
-supports 'fedora'
-
 depends 'yum-epel'
+
+%w(amazon centos fedora oracle redhat scientific).each do |os|
+  supports os
+end
+
+source_url 'https://github.com/chef-cookbooks/yum-remi' if respond_to?(:source_url)
+issues_url 'https://github.com/chef-cookbooks/yum-remi/issues' if respond_to?(:issues_url)
