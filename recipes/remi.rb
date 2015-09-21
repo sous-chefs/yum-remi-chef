add2f4798f11690924e816c4b5428b14f9f53479
Tim Smith <tsmith84@gmail.com>
2015-09-21 14:23:46 -0700

    Fix copyrights

--- recipes/remi.rb
+++ recipes/remi.rb
@@ -2,7 +2,7 @@
 # Author:: Sean OMeara (<sean@chef.io>)
 # Recipe:: yum-remi-chef::remi-php54
 #
-# Copyright 2015, Chef
+# Copyright 2015, Chef Software, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
