2c9e1cd3c794120ed909f54ff983e320db794b37
Andrew Miller <andrew.miller@rakuten.com>
2016-07-26 15:31:19 +0900

    Changed from using ```baseurl``` to ```mirror list```.

--- attributes/remi-php70.rb
+++ attributes/remi-php70.rb
@@ -6,18 +6,22 @@ default['yum']['remi-php70']['managed'] = true
 
 case node['platform']
 when 'fedora'
-  default['yum']['remi-php70']['baseurl'] = "http://rpms.remirepo.net/fedora/#{node['platform_version'].to_i}/php70/$basearch/"
+  # default['yum']['remi-php70']['baseurl'] = "http://rpms.remirepo.net/fedora/#{node['platform_version'].to_i}/php70/$basearch/"
+  default['yum']['remi-php70']['mirrorlist'] = "http://rpms.remirepo.net/fedora/#{node['platform_version'].to_i}/php70/mirror"
   default['yum']['remi-php70']['description'] = "Remi's PHP 7.0 RPM repository for Fedora Linux #{node['platform_version'].to_i} - $basearch"
 else
   case node['platform_version'].to_i
   when 5
-    default['yum']['remi-php70']['baseurl'] = 'http://rpms.remirepo.net/enterprise/5/php70/$basearch/'
+    # default['yum']['remi-php70']['baseurl'] = 'http://rpms.remirepo.net/enterprise/5/php70/$basearch/'
+    default['yum']['remi-php70']['mirrorlist'] = 'http://rpms.remirepo.net/enterprise/5/php70/mirror'
     default['yum']['remi-php70']['description'] = "Remi's PHP 7.0 RPM repository for Enterprise Linux 5 - $basearch"
   when 6, 2013, 2014, 2015, 2016
-    default['yum']['remi-php70']['baseurl'] = 'http://rpms.remirepo.net/enterprise/6/php70/$basearch/'
+    # default['yum']['remi-php70']['baseurl'] = 'http://rpms.remirepo.net/enterprise/6/php70/$basearch/'
+    default['yum']['remi-php70']['mirrorlist'] = 'http://rpms.remirepo.net/enterprise/6/php70/mirror'
     default['yum']['remi-php70']['description'] = "Remi's PHP 7.0 RPM repository for Enterprise Linux 6 - $basearch"
   when 7
-    default['yum']['remi-php70']['baseurl'] = 'http://rpms.remirepo.net/enterprise/7/php70/$basearch/'
+    # default['yum']['remi-php70']['baseurl'] = 'http://rpms.remirepo.net/enterprise/7/php70/$basearch/'
+    default['yum']['remi-php70']['mirrorlist'] = 'http://rpms.remirepo.net/enterprise/7/php70/mirror'
     default['yum']['remi-php70']['description'] = "Remi's PHP 7.0 RPM repository for Enterprise Linux 7 - $basearch"
   end
 end
--- attributes/remi-test.rb
+++ attributes/remi-test.rb
@@ -6,18 +6,22 @@ default['yum']['remi-test']['managed'] = false
 
 case node['platform']
 when 'fedora'
-  default['yum']['remi-test']['baseurl'] = "http://rpms.remirepo.net/fedora/#{node['platform_version'].to_i}/remi-test/$basearch/"
+  # default['yum']['remi-test']['baseurl'] = "http://rpms.remirepo.net/fedora/#{node['platform_version'].to_i}/remi-test/$basearch/"
+  default['yum']['remi-test']['mirrorlist'] = "http://rpms.remirepo.net/fedora/#{node['platform_version'].to_i}/remi-test/mirror"
   default['yum']['remi-test']['description'] = "Remi's test RPM repository for Fedora Linux #{node['platform_version'].to_i} - $basearch"
 else
   case node['platform_version'].to_i
   when 5
-    default['yum']['remi-test']['baseurl'] = 'http://rpms.remirepo.net/enterprise/5/remi-test/$basearch/'
+    # default['yum']['remi-test']['baseurl'] = 'http://rpms.remirepo.net/enterprise/5/remi-test/$basearch/'
+    default['yum']['remi-test']['mirrorlist'] = 'http://rpms.remirepo.net/enterprise/5/remi-test/mirror'
     default['yum']['remi-test']['description'] = "Remi's test RPM repository for Enterprise Linux 5 - $basearch"
   when 6, 2013, 2014, 2015, 2016
-    default['yum']['remi-test']['baseurl'] = 'http://rpms.remirepo.net/enterprise/6/remi-test/$basearch/'
+    # default['yum']['remi-test']['baseurl'] = 'http://rpms.remirepo.net/enterprise/6/remi-test/$basearch/'
+    default['yum']['remi-test']['mirrorlist'] = 'http://rpms.remirepo.net/enterprise/6/remi-test/mirror'
     default['yum']['remi-test']['description'] = "Remi's test RPM repository for Enterprise Linux 6 - $basearch"
   when 7
-    default['yum']['remi-test']['baseurl'] = 'http://rpms.remirepo.net/enterprise/7/remi-test/$basearch/'
+    # default['yum']['remi-test']['baseurl'] = 'http://rpms.remirepo.net/enterprise/7/remi-test/$basearch/'
+    default['yum']['remi-test']['mirrorlist'] = 'http://rpms.remirepo.net/enterprise/7/remi-test/mirror'
     default['yum']['remi-test']['description'] = "Remi's test RPM repository for Enterprise Linux 7 - $basearch"
   end
 end
