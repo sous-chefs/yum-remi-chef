4f1d2fdfbb4483d1ce899a70bf46cc8d86b9407d
Tim Smith <tsmith@chef.io>
2016-09-01 11:25:51 -0700

    Copyright and readme updates
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,7 @@ maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
 description "Installs and configures the Remi's yum repository"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.2.0'
 
 depends 'yum', '~> 3.2'
