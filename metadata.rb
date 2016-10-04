9e13ddeec60215e371e3599331443e48fa58c8f7
Tim Smith <tsmith@chef.io>
2016-10-03 23:16:24 -0700

    Replace yum dependency with compat_resource

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description "Installs and configures the Remi's yum repository"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '2.0.0'
 
-depends 'yum', '>= 3.2', '< 5.0'
+depends 'compat_resource', '>= 12.14.1'
 depends 'yum-epel'
 
 %w(amazon centos fedora oracle redhat scientific).each do |os|
@@ -16,4 +16,4 @@ end
 source_url 'https://github.com/chef-cookbooks/yum-remi-chef'
 issues_url 'https://github.com/chef-cookbooks/yum-remi-chef/issues'
 
-chef_version '>= 12'
+chef_version '>= 12.1'
