6feae8bdd76cfe5a5fc6a3dce54853246b0b7425
Tim Smith <tsmith@chef.io>
2017-01-17 21:35:19 -0800

    Testing updates
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -15,4 +15,4 @@ end
 
 source_url 'https://github.com/chef-cookbooks/yum-remi-chef'
 issues_url 'https://github.com/chef-cookbooks/yum-remi-chef/issues'
-chef_version '>= 12.1'
+chef_version '>= 12.1' if respond_to?(:chef_version)
