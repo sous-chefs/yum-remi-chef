c5b6615aa676c127fcbc7b7727e3a3cab83b2531
Tim Smith <tsmith84@gmail.com>
2020-01-05 11:51:13 -0800

    Require Chef 12.15+
    This gives us modern metadata parsing
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -13,4 +13,4 @@ end
 
 source_url 'https://github.com/chef-cookbooks/yum-remi-chef'
 issues_url 'https://github.com/chef-cookbooks/yum-remi-chef/issues'
-chef_version '>= 12.14'
+chef_version '>= 12.15'
