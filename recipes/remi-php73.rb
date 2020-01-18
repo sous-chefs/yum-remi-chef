1cca508166118fec7e733b7b8b8bd7e79be565a2
Tim Smith <tsmith84@gmail.com>
2020-01-18 14:41:41 -0800

    Cookstyle fix
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- recipes/remi-php73.rb
+++ recipes/remi-php73.rb
@@ -16,7 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-unless node['platform'] == 'fedora'
+unless platform?('fedora')
   include_recipe 'yum-epel'
 
   if node['platform_version'].to_i > 5
