857924d68542511ce212dac362a1f9f81dfecb47
Tim Smith <tsmith84@gmail.com>
2020-04-15 10:24:34 -0700

    Remove logic to support EOL Fedora platforms
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- attributes/remi-gpgkey.rb
+++ attributes/remi-gpgkey.rb
@@ -1,11 +1,8 @@
 # From https://rpms.remirepo.net/ in the "Other Resources" section about
 # gpg keys for platform versions.
-default['yum-remi-chef']['gpgkey'] = if node['platform'].eql?('fedora') &&
+default['yum-remi-chef']['gpgkey'] = if platform?('fedora') &&
                                         node['platform_version'].to_i >= 28
                                        'https://rpms.remirepo.net/RPM-GPG-KEY-remi2018'
-                                     elsif node['platform'].eql?('fedora') &&
-                                           node['platform_version'].to_i.between?(26, 27)
-                                       'https://rpms.remirepo.net/RPM-GPG-KEY-remi2017'
                                      else
                                        'https://rpms.remirepo.net/RPM-GPG-KEY-remi'
                                      end
